92102dbd40b4a8adb9633c123645246e51e0f816
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2025-04-12 15:10:16 +0200

    Eureka service

--- product-microservice/src/main/java/com/econofood/product_microservice/ProductMicroserviceApplication.java
+++ product-microservice/src/main/java/com/econofood/product_microservice/ProductMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.econofood.product_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductMicroserviceApplication {
 
