c9101a88b404b844f0d651310ed3a09c763e37f2
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2025-04-13 12:04:39 +0200

    Admin

---
+++ admin-service/src/test/java/econofood/admin_service/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package econofood.admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
